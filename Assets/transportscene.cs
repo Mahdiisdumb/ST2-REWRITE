aa8c4874b0d642214140d46c958fccfd7445f979
Mahdiisdumb <mahdihamade6789010@outlook.com>
2026-09-24 18:36:20 -0400

    fsd

---
+++ Assets/transportscene.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class transportscene : MonoBehaviour
+{
+	public int scene;
+	public float time;
+
+	void Update()
+	{
+		time -= Time.deltaTime;
+
+		if (time <= 0)
+		{
+			SceneManager.LoadScene(scene);
+		}
+	}
+}
